63c4d2b4f81b03fbc8038c7ab9387412aec15696
Afsara Tasnim <tratina157@gmail.com>
2023-08-08 16:04:02 +0600

    pagi serach

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,9 +1,9 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-import Searchbar from "@/components/shared/Searchbar";
-import Pagination from "@/components/shared/Pagination";
-import CommunityCard from "@/components/cards/CommunityCard";
+// import Searchbar from "@/components/shared/Searchbar";
+// import Pagination from "@/components/shared/Pagination";
+// import CommunityCard from "@/components/cards/CommunityCard";
 
 import { fetchUser } from "@/lib/actions/user.actions";
 import { fetchCommunities } from "@/lib/actions/community.actions";
